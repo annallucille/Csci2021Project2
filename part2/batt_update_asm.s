c063ed2eee945bfaccbe9208e992b345626c6d8e
michael-oberpriller <93411352+michael-oberpriller@users.noreply.github.com>
2023-03-24 14:50:52 -0500

    Update batt_update_asm.s

--- part2/batt_update_asm.s
+++ part2/batt_update_asm.s
@@ -42,12 +42,141 @@ my_array:                   # declare multiple ints sequentially starting at loc
 .global  set_display_from_batt
 
 # ENTRY POINT FOR REQUIRED FUNCTION
-set_display_from_batt:  
-    ## assembly instructions here
+set_display_from_batt:
+    movq %rdi, %rax                            # %rax = batt
+    shr $24, %rax                              # %al = batt.mode
+    cmp $1, %al                                # if (batt.mode == 1)
+    je percent                                 #     goto percent
+    cmp $2, %al                                # else if (batt.mode == 2)
+    je voltage                                 #     goto voltage
+    movq $1, %rax                              # else return 1
+    ret
 
-	## two useful techniques for this problem
-    movl    my_int(%rip),%eax    # load my_int into register eax
-    leaq    my_array(%rip),%rdx  # load pointer to beginning of my_array into rdx
+# data = %ax
+# data_temp = %cx
+# result = %r8d
+# temp = %r11w
+# index = %rdx
+# seven_segment_bitmask = %r10
+# digit = %r9d
+percent:
+    movl $1, %r8d                              # result = 0b1
+    movq %rdi, %rax                            # %rax = batt
+    shr $16, %rax                              # %al = batt.percent
+    movsbw %al, %ax                            # data = batt.percent
+    leaq seven_segment_bitmask(%rip), %r10     # %r10 = seven_segement_bitmask
+    
+    # Right
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= temp; index = data % temp
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $3, %r9d                              # digit = digit << 3
+    addl %r9d, %r8d                            # result += digit
+    
+    # Middle
+    movw %ax, %cx                              # data_temp = data
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= temp; index = data % temp
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $10, %r9d                             # digit = digit << 10
+    movl $0, %r11d                             # temp = 0
+    cmp $0, %cx                                # if data_temp == 0
+    cmove %r11d, %r9d                          #     digit = 0
+    addl %r9d, %r8d                            # result += digit
+
+    # Left
+    movw %ax, %cx                              # data_temp = data
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= temp; index = data % temp
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $17, %r9d                             # digit = digit << 17
+    movl $0, %r11d                             # temp = 0
+    cmp $0, %cx                                # if data_temp == 0
+    cmove %r11d, %r9d                          #     digit = 0
+    addl %r9d, %r8d                            # result += digit
+
+    jmp both                                   # goto both
+
+# data = %ax
+# result = %r8d
+# temp = %r11w
+# index = %edx
+# seven_segment_bitmask = %r10
+# digit = %r9d
+# lastDigit = %rdx
+# round = %r9w
+voltage:
+    movl $6, %r8d                              # result = 0b110
+    movw %di, %ax                              # data = batt.mlvolts
+    leaq seven_segment_bitmask(%rip), %r10     # %r10 = seven_segement_bitmask
+
+    # Rounding
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= 10; lastDigit = data % 10
+    movw $0, %r9w                              # round = 0
+    movw $1, %r11w                             # temp = 1
+    cmp $5, %rdx                               # if lastDigit >= 5
+    cmovge %r11w, %r9w                         #      round = 1
+    addw %r9w, %ax                             # data += round
+    
+    # Right
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= 10; index = data % 10
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $3, %r9d                              # digit == digit << 3
+    addl %r9d, %r8d                            # result += digit
+    
+    # Middle
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= 10; index = data % 10
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $10, %r9d                             # digit == digit << 10
+    addl %r9d, %r8d                            # result += digit
+
+    # Left
+    movw $10, %r11w                            # temp = 10
+    cqto
+    idivw %r11w                                # data /= 10; index = data % 10
+    movl (%r10, %rdx, 4), %r9d                 # digit = seven_segement_bitmask[index]
+    shll $17, %r9d                             # digit == digit << 17
+    addl %r9d, %r8d                            # result += digit
+
+    jmp both                                   # goto both
+
+# batteryLevel = %r9d
+# result = %r8d
+# percent = %al
+# temp = %r11d
+both:
+    movq %rdi, %rax                            # %rax = batt
+    shrq $16, %rax                             # percent = batt.percent
+    movl $0, %r9d                              # batteryLevel = 0
+    movl $0x1000000, %r11d                     # temp = 0x1000000
+    cmp $5, %al                                # if percent >= 5
+    cmovge %r11d, %r9d                         #     batteryLevel = 0x1000000
+    addl %r9d, %r8d                            # result += batteryLevel
+
+    subb $10, %al                              # percent -= 10
+    movsbq %al, %rax
+    cqto
+    movb $20, %r11b                            # temp = 20
+    idivb %r11b                                # percent /= 20
+    movb %al, %cl                              # %cl = %al
+    movl $1, %r9d                              # batteryLevel = 1
+    shll %cl, %r9d                             # batteryLevel = batteryLevel << percent
+    subl $1, %r9d                              # batteryLevel -= 1
+    shll $25, %r9d                             # batteryLevel = batteryLevel << 25
+    addl %r9d, %r8d                            # result += batteryLevel
+
+    movl %r8d, (%rsi)                          # *display = result
+    movq $0, %rax                              # return 0
+    ret
 
 
 .text
